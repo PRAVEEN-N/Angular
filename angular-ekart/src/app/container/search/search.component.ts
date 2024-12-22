03cc5f9bc3b73a12e0836e49e7f72c02d45c21ec
PRAVEEN NAGULA <praveennagula1999@gmail.com>
2024-12-22 21:07:26 +0530

    @Viewchild Usage to directly access input Element

--- angular-ekart/src/app/container/search/search.component.ts
+++ angular-ekart/src/app/container/search/search.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Output, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-search',
@@ -11,8 +11,10 @@ export class SearchComponent {
   @Output()
   searchProductText:EventEmitter<string> = new EventEmitter<string>();
 
-  onSearchProductText(searchInput:HTMLInputElement){
-    this.searchText = searchInput.value;
+  @ViewChild('searchInput')
+  selectedElementRef: ElementRef;
+  onSearchProductText(){
+    this.searchText = this.selectedElementRef.nativeElement.value;
     this.searchProductText.emit(this.searchText);
   }
 
