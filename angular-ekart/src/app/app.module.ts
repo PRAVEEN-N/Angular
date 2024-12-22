753939bfe0d1de3db2e357b4f15ce29032a50596
PRAVEEN NAGULA <praveennagula1999@gmail.com>
2024-12-22 18:22:14 +0530

    ngFor for complext types

--- angular-ekart/src/app/app.module.ts
+++ angular-ekart/src/app/app.module.ts
@@ -6,8 +6,9 @@ import { HeaderComponent } from './header/header.component';
 import { TopHeaderComponent } from './top-header/top-header.component';
 import { TopMenuComponent } from './header/top-menu/top-menu.component';
 import { MainMenuComponent } from './header/main-menu/main-menu.component';
-import { SearchComponent } from './product-list/search/search.component';
-import { ProductListComponent } from './product-list/product-list.component';
+import { SearchComponent } from './container/search/search.component';
+import { ContainerComponent } from './container/container.component';
+import { ProductListComponent } from './container/product-list/product-list.component';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
@@ -18,7 +19,8 @@ import { FormsModule } from '@angular/forms';
     TopMenuComponent,
     MainMenuComponent,
     SearchComponent,
-    ProductListComponent
+    ProductListComponent,
+    ContainerComponent
   ],
   imports: [
     BrowserModule,
