12feeadf7a8b14b293d4dcca1c8cd71b6c361fda
PRAVEEN NAGULA <praveennagula1999@gmail.com>
2024-12-24 12:02:41 +0530

    ngOnChanges hook

--- angular-lifecycle-hooks/src/app/app.component.ts
+++ angular-lifecycle-hooks/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,8 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   title = 'angular-lifecycle-hooks';
   constructor() {
     console.log("App Constructor");
   }
+  inputVal:string = '';
+  onSubmit(input: HTMLInputElement ) {
+    this.inputVal = input.value;
+  }
+
 }
--- angular-lifecycle-hooks/src/app/demo/demo.component.ts
+++ angular-lifecycle-hooks/src/app/demo/demo.component.ts
@@ -1,18 +1,22 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-demo',
   templateUrl: './demo.component.html',
   styleUrls: ['./demo.component.css']
 })
-export class DemoComponent {
+export class DemoComponent implements OnChanges{
 
   name:string = '';
 
   @Input()
-  message:string = 'Hello';
+  message:string = '';
   constructor() {
     console.log("Demo Constructor");
     console.log('Message', this.message) // prints hello, but we are passing this data from parent component. Input properties not injected at initialization (Executing contructor).
+  }
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('changes', changes);
+    console.log('mesage', this.message);
   }
 }
