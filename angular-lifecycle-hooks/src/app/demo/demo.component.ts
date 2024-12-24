f06c98d90ce27e59f2d226f48bd2af36a4a34b53
PRAVEEN NAGULA <praveennagula1999@gmail.com>
2024-12-24 11:40:24 +0530

    Updated comment

--- angular-lifecycle-hooks/src/app/demo/demo.component.ts
+++ angular-lifecycle-hooks/src/app/demo/demo.component.ts
@@ -13,6 +13,6 @@ export class DemoComponent {
   message:string = 'Hello';
   constructor() {
     console.log("Demo Constructor");
-    console.log('Message', this.message)
+    console.log('Message', this.message) // prints hello, but we are passing this data from parent component. Input properties not injected at initialization (Executing contructor).
   }
 }
