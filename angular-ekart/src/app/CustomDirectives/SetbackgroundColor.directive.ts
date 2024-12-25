04e4b678648fbed515ba43e60adb9b076f94f14c
PRAVEEN NAGULA <praveennagula1999@gmail.com>
2024-12-25 16:07:45 +0530

    Property Binding in Directives : Sending multiple values from view

--- angular-ekart/src/app/CustomDirectives/SetbackgroundColor.directive.ts
+++ angular-ekart/src/app/CustomDirectives/SetbackgroundColor.directive.ts
@@ -7,11 +7,11 @@ import { Directive, ElementRef, Input, OnInit, Renderer2 } from "@angular/core";
 )
 export class SetBackGroundColor implements OnInit{
 
-    @Input() backColor: string = '#36454F';
+    // @Input() backColor: string = '#36454F';
+    // @Input('textColor') color: string = 'white';
 
-
-    @Input('textColor') color: string = 'white';
-
+    @Input()
+    setBackGroundColor ={backColor: '#36454F', color: 'white'}; // Setting multiple values using object
     // private renderer: Renderer2;
     constructor(private element:ElementRef, private renderer: Renderer2) {
         // this.renderer = renderer;
@@ -19,7 +19,7 @@ export class SetBackGroundColor implements OnInit{
     ngOnInit(): void {
         // this.element.nativeElement.style.backgroundColor = '#36454F';
         // this.element.nativeElement.style.color = 'white'
-        this.renderer.setStyle(this.element.nativeElement, 'backgroundColor', this.backColor);
-        this.renderer.setStyle(this.element.nativeElement, 'color', this.color);
+        this.renderer.setStyle(this.element.nativeElement, 'backgroundColor', this.setBackGroundColor.backColor);
+        this.renderer.setStyle(this.element.nativeElement, 'color', this.setBackGroundColor.color);
     }
 }
