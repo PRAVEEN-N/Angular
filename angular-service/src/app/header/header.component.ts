7621ef1a3380411021da7a0a9c6c00ba14425568
PRAVEEN NAGULA <praveennagula1999@gmail.com>
2024-12-26 18:45:01 +0530

    use of  inject(serviceInstance)

--- angular-service/src/app/header/header.component.ts
+++ angular-service/src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject } from '@angular/core';
+import { Component, inject, Inject } from '@angular/core';
 import { Subscribe } from '../Services/subscribe.service';
 
 @Component({
@@ -10,10 +10,10 @@ import { Subscribe } from '../Services/subscribe.service';
 export class HeaderComponent {
   selectedTab: string = 'home';
 
-  constructor(private service: Subscribe) {
+  // constructor(private service: Subscribe) {
 
-  }
-
+  // }
+  service = inject(Subscribe);
   //When HOME Link is clicked
   HomeClicked(){
     this.selectedTab = 'home';
