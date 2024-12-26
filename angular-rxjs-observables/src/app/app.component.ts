1bc98cdac3cbb05912771e0aeab725708873f78c
PRAVEEN NAGULA <praveennagula1999@gmail.com>
2024-12-26 19:33:25 +0530

    stremData using next

--- angular-rxjs-observables/src/app/app.component.ts
+++ angular-rxjs-observables/src/app/app.component.ts
@@ -12,12 +12,23 @@ export class AppComponent{
   data: any[] = [];
 
   myObervable = new Observable((observer)=> {
-    observer.next([1,2,3,4]);
+    setTimeout(()=> {
+      observer.next(1)
+    }, 1000);
+    setTimeout(()=> {
+      observer.next(2)
+    }, 2000);
+    setTimeout(()=> {
+      observer.next(3)
+    }, 4000);
+    setTimeout(()=> {
+      observer.next(4)
+    }, 5000);
   })
 
   getData() {
     this.myObervable.subscribe((data:any) => {
-      this.data = data;
+      this.data.push(data);
     })
   }
 }
