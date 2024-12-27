6ee94a30f6ef3c627efc9097ecf34aa424e4ad56
PRAVEEN NAGULA <praveennagula1999@gmail.com>
2024-12-27 16:07:21 +0530

    of and from operators in rxJs

--- angular-rxjs-observables/src/app/app.component.ts
+++ angular-rxjs-observables/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Observable } from 'rxjs';
+import { from, Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -11,27 +11,34 @@ export class AppComponent {
 
   data: any[] = [];
 
-  myObervable = new Observable((observer) => {
-    setTimeout(() => {
-      observer.next(1);
-    }, 1000);
-    setTimeout(() => {
-      observer.next(2);
-    }, 2000);
-    setTimeout(() => {
-      observer.next(3);
-    }, 4000);
+  // myObervable = new Observable((observer) => {
     // setTimeout(() => {
-    //   observer.error(new Error('Something went wrong'));
+    //   observer.next(1);
+    // }, 1000);
+    // setTimeout(() => {
+    //   observer.next(2);
+    // }, 2000);
+    // setTimeout(() => {
+    //   observer.next(3);
     // }, 4000);
-    setTimeout(() => {
-      observer.next(4);
-    }, 5000);
-    setTimeout(() => {
-      observer.complete();
-    }, 6000);
+    // // setTimeout(() => {
+    // //   observer.error(new Error('Something went wrong'));
+    // // }, 4000);
+    // setTimeout(() => {
+    //   observer.next(4);
+    // }, 5000);
+    // setTimeout(() => {
+    //   observer.complete();
+    // }, 6000);
+  // });
+  // myObervable = of([1,2,3], true, '1') // to convert any data to observable auto emits completion event
+  // myObervable = from([1,2,3,4,5,6,7,8,9,10]); //  to convert array (iterable )to observable auto emits completion event
+  // myObervable = from(1); // returnns error because the input is not iterable
+  // myObervable = from('hello'); // returnns'h', 'e', 'l', 'l', 'o', 'completed' because the input is iterable (string: sequence of chars)
+  myPromise = new Promise((resolve, reject) => {
+    resolve(1);
   });
-
+  myObervable = from(this.myPromise); // can be used to convert promise to observable
   getData() {
     // this.myObervable.subscribe(
     //   (data: any) => {
