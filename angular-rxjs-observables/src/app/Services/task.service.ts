d68bc48f4fa25954c35b0723056145cc836fada0
PRAVEEN NAGULA <praveennagula1999@gmail.com>
2025-01-02 10:22:41 +0530

    Data Sharing using Subject

--- angular-rxjs-observables/src/app/Services/task.service.ts
+++ angular-rxjs-observables/src/app/Services/task.service.ts
@@ -4,11 +4,14 @@ import { Subject } from 'rxjs';
     providedIn: 'root'
 })
 export class TaskService{
-    CreateTask: EventEmitter<string> = new EventEmitter<string>();
+    // CreateTask: EventEmitter<string> = new EventEmitter<string>();
 
-    // CreateTask = new Subject<string>();
+    CreateTask = new Subject<string>();
 
+    // OnCreateTask(value: string){
+    //     this.CreateTask.emit(value);
+    // }
     OnCreateTask(value: string){
-        this.CreateTask.emit(value);
+        this.CreateTask.next(value);
     }
 }
