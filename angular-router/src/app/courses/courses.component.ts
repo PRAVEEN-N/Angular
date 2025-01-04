2c044c81173ac4ccec322e2f53a1d78f7737e3c9
PRAVEEN NAGULA <praveennagula1999@gmail.com>
2025-01-04 16:41:47 +0530

    Retrieve query string and load component

--- angular-router/src/app/courses/courses.component.ts
+++ angular-router/src/app/courses/courses.component.ts
@@ -6,9 +6,9 @@ import { ActivatedRoute, Router } from '@angular/router';
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
-  styleUrls: ['./courses.component.css']
+  styleUrls: ['./courses.component.css'],
 })
-export class CoursesComponent implements OnInit{
+export class CoursesComponent implements OnInit {
   coursesService = inject(CourseService);
   AllCourses: Course[];
   activedRoute: ActivatedRoute = inject(ActivatedRoute);
@@ -18,12 +18,23 @@ export class CoursesComponent implements OnInit{
   //   this.router.navigate(['Courses/Course', courseId]) ;
   // }
   ngOnInit(): void {
-    let searchText = this.activedRoute.snapshot.queryParams['search'];
-    if(searchText) {
-      console.log(searchText);
-      this.AllCourses = this.coursesService.courses.filter(c=> c.title.toLowerCase().includes(searchText.toLowerCase()));
-    } else {
-      this.AllCourses = this.coursesService.courses;
-    }
+    // let searchText = this.activedRoute.snapshot.queryParams['search'];
+    // if(searchText) {
+    //   console.log(searchText);
+    //   this.AllCourses = this.coursesService.courses.filter(c=> c.title.toLowerCase().includes(searchText.toLowerCase()));
+    // } else {
+    //   this.AllCourses = this.coursesService.courses;
+    // }
+    this.activedRoute.queryParamMap.subscribe((params) => {
+      let searchText = params.get('search');
+      if (searchText) {
+        console.log(searchText);
+        this.AllCourses = this.coursesService.courses.filter((c) =>
+          c.title.toLowerCase().includes(searchText.toLowerCase())
+        );
+      } else {
+        this.AllCourses = this.coursesService.courses;
+      }
+    });
   }
 }
